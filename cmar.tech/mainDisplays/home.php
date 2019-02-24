48d82259722ea9979ee47a78c14e417f87d605b1
Cramer Smith <runner.cram@gmail.com>
2019-02-24 01:33:14 -0800

    Added my picture to the header, and added a .htaccess file allowing for the differing URL to no longer cause hanging program

--- cmar.tech/mainDisplays/home.php
+++ cmar.tech/mainDisplays/home.php
@@ -1,32 +1,33 @@
 <?php
 	include_once("../../resources/config.php");
 
-	function DOMinnerHTML(DOMNode $element) 
-	{ 
-		$innerHTML = ""; 
-		$children  = $element->childNodes;
+	function curl_get_file_contents($URL)
+    {
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_URL, $URL);
+        $contents = curl_exec($c);
+        curl_close($c);
 
-		foreach ($children as $child) 
-		{ 
-			$innerHTML .= $element->ownerDocument->saveHTML($child);
-		}
-
-		return $innerHTML; 
-	}
+        if ($contents) return $contents;
+            else return FALSE;
+    }
 
 	// XKCD
-	$currentXKCD = json_decode(file_get_contents("https://xkcd.com/info.0.json"));
-	
+	$currentXKCD = json_decode(curl_get_file_contents("https://xkcd.com/info.0.json"));
+
 	// sketch daily
-	$sketchDailyObject = json_decode(file_get_contents("https://www.reddit.com/r/SketchDaily.json"));
+
+	$sketchDailyObject = json_decode(curl_get_file_contents("https://www.reddit.com/r/SketchDaily.json"));
 	$todaysSketch = $sketchDailyObject->data->children[1]->data;
 	$sketchSmallSelfText = ucfirst(explode("alt theme:", strtolower($todaysSketch->selftext))[0]);
 
 	// Hacker News
-	$hackerNewsData = file_get_contents("https://news.ycombinator.com/");
-	$dom = new DOMDocument();
-	$dom->loadHTML(explode('</table>', explode('<table border="0" cellpadding="0" cellspacing="0" class="itemlist">', $hackerNewsData)[1])[0]);
-	$childNodeList = $dom->getElementsByTagName('td');
+	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+	$hackerNewsData = curl_get_file_contents("https://news.ycombinator.com/");
+	$doc = new DOMDocument();
+	$doc->loadHTML(explode('</table>', explode('<table border="0" cellpadding="0" cellspacing="0" class="itemlist">', $hackerNewsData)[1])[0]);
+	$childNodeList = $doc->getElementsByTagName('td');
 	$className = "title";
 	$hackerNewsTop = "<ol>";
 	$hackerAmount = 5;
@@ -34,7 +35,11 @@
 		$temp = $childNodeList->item($i);
 		if (stripos($temp->getAttribute('class'), $className) !== false) {
 			if ($temp->firstChild->nodeName == 'a') {
-				$hackerNewsTop .= "<li><a href='" . $temp->firstChild->getAttribute('href'). "'>" . $temp->firstChild->textContent .  "</a></li>";
+				$title = ($temp->firstChild->textContent);
+				$title = iconv('UTF-8', 'ISO-8859-1//TRANSLIT', $title);
+				$URL = $temp->firstChild->getAttribute('href');
+				
+				$hackerNewsTop .= "<li><a href='" . $URL . "' target='_blank'>" . $title .  "</a></li>";
 				$hackerAmount --;
 				if ($hackerAmount == 0) {
 					break;
@@ -45,10 +50,12 @@
 	$hackerNewsTop.="</ol>";
 
 	// daily programming
-	$dailyChallengeData = json_decode(file_get_contents("https://www.reddit.com/r/dailyprogrammer.json"));
+	$dailyChallengeData = json_decode(curl_get_file_contents("https://www.reddit.com/r/dailyprogrammer.json"));
 	$todaysChallenge = $dailyChallengeData->data->children[0]->data;
 	$todaysChallendeLink = "<a href='" . $todaysChallenge->url . "'>" . $todaysChallenge->title ."</a>"; 
 
+	// TODO: Grab a podcast from an RSS feed
+	//  
 
 ?>
 
@@ -56,7 +63,7 @@
 
 <div id=mainContentContainer>
 	<div id=hackerNews class=homeContentSection>
-		 <a href='https://news.ycombinator.com/'><h3>Haker News</h3></a>   
+		 <a href='https://news.ycombinator.com/'><h3>Hacker News</h3></a>   
 		<p>The current top 5 articles</p>
 		<div id=hackerNewsDisplay class="homeContentDiplay">
 			<?=$hackerNewsTop?>
@@ -70,6 +77,7 @@
 			<div>
 				<span><?=$currentXKCD->safe_title?>: <span class="infoDate"><?=$currentXKCD->month."/".$currentXKCD->day."/".$currentXKCD->year?></span></span>
 				<img src=<?=$currentXKCD->img?> alt='<?echo($currentXKCD->alt)?>'/>
+				<br><h4>Alt Text:</h4><span><?echo($currentXKCD->alt)?></span>
 			</div>
 		</div>
 	</div>
--- resources/templates/display.php
+++ resources/templates/display.php
@@ -20,28 +20,33 @@ class Display
 		?>
 	
 		<div id="mainHeader">
-			
-			<h1 id="headerTitle"><?=$title?></h1>
-			
-			<p id="headerDescription"> 
-				<?=$description?>
-			</p>
-
-			<div id="header_social">
-				<a class='socialLink' href=<?=$config['urls']['linkedin']?>> <img src=https://cmar.tech/images/layout/linkedinRecroppedLogo-5292ff.png alt="LinkedIn"></a>
-				<a class='socialLink' href=<?=$config['urls']['github']?>><img src="https://cmar.tech/images/layout/GitHub-Mark-120px-plus-5292ff.png"></a>
-				<a class='socialLink' href=<?=$config['urls']['twitter']?>><img src=https://cmar.tech/images/layout/twitter-5292ff.png alt="Twitter"></a>
+			<div id=headerImageContainer>
+				<img id=headerMainImage src='<?=$config['paths']['images']['content'] . "/handome_young_man.jpg"?>' alt="Handsome Young Man">
 			</div>
+			<div id=headerTextContainer>
+				<span><h1 id="headerTitle"><?=$title?></h1></span>
+				
+				<p id="headerDescription"> 
+					<?=$description?>
+				</p>
 
-			<div id=navButtonsContainer>
-				<nav>
-					<button id=homeButton class='navigationButton' value=<?=$config['home']?>>Home</button>
-					<button class='navigationButton' value=<?=$config['bio']?>>About</button>
-					<button class='navigationButton' value=<?=$config['projects']?>>Projects</button>
-					<button class='navigationButton' value=<?=$config['resumePage']?>>Resume</button>
-					<!-- <button class='navigationButton' value=//$config['blog']>Blog</button> -->
-				</nav>
+				<div id="header_social">
+					<a class='socialLink' href=<?=$config['urls']['linkedin']?>> <img src=https://cmar.tech/images/layout/linkedinRecroppedLogo-5292ff.png alt="LinkedIn"></a>
+					<a class='socialLink' href=<?=$config['urls']['github']?>><img src="https://cmar.tech/images/layout/GitHub-Mark-120px-plus-5292ff.png"></a>
+					<a class='socialLink' href=<?=$config['urls']['twitter']?>><img src=https://cmar.tech/images/layout/twitter-5292ff.png alt="Twitter"></a>
+				</div>
+
+				<div id=navButtonsContainer>
+					<nav>
+						<button id=homeButton class='navigationButton' value=<?=$config['home']?>>Home</button>
+						<button class='navigationButton' value=<?=$config['bio']?>>About Me</button>
+						<button class='navigationButton' value=<?=$config['projects']?>>Projects</button>
+						<button class='navigationButton' value=<?=$config['resumePage']?>>Resume</button>
+						<!-- <button class='navigationButton' value=//$config['blog']>Blog</button> -->
+					</nav>
+				</div>
 			</div>
+			<div class="clear"></div>
 		</div>
 		<?php
 	}
@@ -68,6 +73,7 @@ class Display
 				<title><?=$title?></title>
 				<link type="text/css" rel="stylesheet" href="<?=$config['paths']['stylesheets']."/main.css"?>"/>
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+				<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.2.3/dist/sweetalert2.all.min.js"></script>
 				<script src="<?=$config['paths']['javascript']."/home.js"?>"></script>
 			</head>
 
@@ -177,9 +183,10 @@ class Display
 				<div id=apiList>
 					<h4 id=footerAPISubheader>Tools Used:</h4>
 					<nav id=footerAPILinks>
-						<a class=footerAPILink href="https://highlightjs.org">highlighter.js</a>
-						<a class=footerAPILink href="https://www.google.com/analytics/">google analytics</a>
+						<a class=footerAPILink href="https://highlightjs.org">HighLighter.js</a>
+						<a class=footerAPILink href="https://www.google.com/analytics/">Google Analytics</a>
 						<a class=footerAPILink href="https://jquery.com/">jQuery</a>
+						<a class=footerAPILink href="https://sweetalert2.github.io/">SweetAlert2</a>
 					</nav>
 				</div>
 
